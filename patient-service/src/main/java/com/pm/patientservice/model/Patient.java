80fa46ed97a15425e0262fe27ad1652f8a74dac6
Tan Cao <tancaodev.github@gmail.com>
2025-05-29 22:30:12 +0700

    feat: Create patient data flow (controller -> service -> repository -> db)

--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -29,7 +29,7 @@ public class Patient {
     private LocalDate dateOfBirth;
 
     @NotNull
-    private LocalDate registerDate;
+    private LocalDate registeredDate;
 
     public UUID getId() {
         return id;
@@ -71,12 +71,12 @@ public class Patient {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public LocalDate getRegisterDate() {
-        return registerDate;
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
     }
 
-    public void setRegisterDate(LocalDate registerDate) {
-        this.registerDate = registerDate;
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
     }
 
 }
