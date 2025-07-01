c52a36ae65481bfe31f65c365c485e4e9ddbbe3d
Tan Cao <tancaodev.github@gmail.com>
2025-07-01 23:19:17 +0700

    feat: create and connect auth service from controller -> auth service -> user service -> user repository

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private final UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<User> user = userService.findByEmail(loginRequestDTO.getEmail());
+    }
+}
